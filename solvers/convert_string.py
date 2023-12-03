bf3142cf5bb001157a02756b5f9b2152d921009c
Martijn Elands <m.elands@student.maastrichtuniversity.nl>
2023-12-03 22:47:03 +0100

    Example file of converting string fractions to FloatDP

---
+++ solvers/convert_string.py
@@ -0,0 +1,16 @@
+# Example file to convert a string, such as "1/2" to a FloatDP
+
+from pyariadne import FloatDP, dp
+
+fraction_string = "1/2"
+# fl = FloatDP(s, dp) #gives error, so we need another way to convert it
+numerator, denominator = fraction_string.split("/")
+print(numerator)
+print(denominator)
+first = FloatDP(numerator, dp)
+second = FloatDP(denominator, dp)
+
+together = first/second
+together = together.value() # An approximation to the actual value
+print(together) # is a FloatDP
+print(type(together))
