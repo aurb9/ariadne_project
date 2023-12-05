82886133f69d6d0aeeb2c207bfa96a3f40e5b1ea
Martijn Elands <m.elands@student.maastrichtuniversity.nl>
2023-12-05 19:23:01 +0100

    Updates examples and clarified naming

--- solvers/box_division.py
+++ solvers/box_division.py
@@ -1,65 +1,57 @@
-##EXAMPLE FILE IN PROGRESS OF GETTING A BOX DIVISION BY 1/BOX
+# EXAMPLE FILE IN PROGRESS OF GETTING A BOX DIVISION BY 1/BOX
+from typing import Tuple
+from pyariadne import BoxDomainType, FloatDP, dp, FloatDPExactInterval
 
-from pyariadne import BoxDomainType, FloatDP, dp
-
-
+ZERO = FloatDP(0, dp)
+ONE = FloatDP(1, dp)
 INF = FloatDP.inf(dp)
-#B=BoxDomainType([("0.5", INF)]) #case 1
-#B=BoxDomainType([("-0.5", INF)]) #case 2
-B = BoxDomainType([(-INF, "-0.5")]) #case 3
 
+
+def _convert_single_box(subdomain: FloatDPExactInterval) -> Tuple[FloatDP, FloatDP]:
+    """
+    Convert a single box by 1/box. E.g., box=[-5, 10] --> new_box = [-1/5, 1/10]
+    :param subdomain: an interval with a lowerbound and upperbound in the original domain
+    :return: tuple of (new_lowerbound and new_upperbound) for the creation of a new box
+    """
+    lower_bound = subdomain.lower_bound()
+    upper_bound = subdomain.upper_bound()
+    if lower_bound > ZERO and upper_bound > ZERO:
+        l_new = ONE / upper_bound
+        u_new = ONE / lower_bound
+    elif lower_bound < ZERO < upper_bound:
+        l_new = ONE / lower_bound
+        u_new = ONE / upper_bound
+    else:
+        l_new = ONE / upper_bound
+        u_new = ONE / lower_bound
+    l_new = l_new.value()
+    u_new = u_new.value()
+
+    return l_new, u_new
+
+
+def _convert_domains(domain: BoxDomainType) -> BoxDomainType:
+    """
+    Converting the entire domain to 1/domain by going over every subdomain and converting that
+    :param domain: a box containing the domain (can be single or multiple)
+    :return: the new_domain which is 1/input_domain
+    """
+    subdomains = []
+    for dim_i in range(domain.dimension()):
+        subdomains.append(_convert_single_box(domain[dim_i]))
+    new_domain = BoxDomainType(subdomains)
+
+    return new_domain
+
+
+case1 = (-INF, "-5")  # unbounded domain, strictly negative
+case2 = (-INF, "5")  # unbounded domain, containing zero
+case3 = ("-5", INF)  # unbounded domain, containing zero
+case4 = ("5", INF)  # unbounded domain, strictly positive
+# case5 = (-INF, INF)  # unbounded domain, containing 0
+B = BoxDomainType([case1, case2, case3, case4])
 print('Original box', B)
 
 
-l = B[0].lower_bound()
-u = B[0].upper_bound()
-
-#if LOWER_BOUND > 0:
-#   Therefore, the UPPER_BOUND > 0, so:
-#      LOWER_BOUND --> NEW_UPPER_BOUND
-#      AND
-#      UPPER_BOUND --> NEW_LOWER_BOUND
-if l > ZERO and u > ZERO:
-    # if l > FloatDP(0, dp): #can be converted later, but since u<l we need this check
-    print('Case 1')
-    NEW_l = ONE/u
-    NEW_u = ONE/l
-    #print(type(NEW_u.lower())) #get lowerbound type
-    #print(NEW_u.lower()) #get lowebound
-    #print(NEW_u.lower().raw()) #get float data of lb
-    #print(type(NEW_u.lower().raw())) #FloatDP
-    NEW_u = NEW_u.lower().raw()
-    NEW_l = NEW_l.lower().raw()
-
-    #print(NEW_u)
-    #print(NEW_l)
-    B_prime = BoxDomainType([(NEW_l, NEW_u)])
-    print('New box:', B_prime)
-elif l < ZERO < u:
-    print('Case 2')
-    #E.g. [-1, 2] --> 1/-1, 1/2 --> -1, 1/2 --> [-1, 1/2]
-    #E.g. [-10, 5] --> 1/-10, 1/5 --> -1/10, 1/5
-    #Since l<0, 1/l stays negative
-    #Since u>0, 1/u stays positive
-    NEW_l = ONE/l
-    NEW_u = ONE/u
-
-    NEW_u = NEW_u.lower().raw()
-    NEW_l = NEW_l.lower().raw()
-
-    B_prime = BoxDomainType([(NEW_l, NEW_u)])
-    print('New box:', B_prime)
-elif l < ZERO and u < ZERO:
-    print('Case 3')
-    #E.g. [-10, -2] --> -1/10, -1/2 so l and u get swapped
-
-    NEW_l = ONE/u
-    NEW_u = ONE/l
-
-    NEW_u = NEW_u.lower().raw()
-    NEW_l = NEW_l.lower().raw()
-
-    B_prime = BoxDomainType([(NEW_l, NEW_u)])
-    print('New box:', B_prime)
-else:
-    print('Not identified case, please resolve')
+B_prime = _convert_domains(domain=B)
+print('New box:', B_prime)
